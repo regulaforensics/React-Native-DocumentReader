cd5105a13c73e87936721ab37effbf52d2593046
Павел Масюк <masukpasha@gmail.com>
2020-09-01 15:40:59 +0300

    new sample

--- DocumentReader/App.js
+++ DocumentReader/App.js
@@ -1,38 +1,41 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Button, Text, Image, ScrollView, NativeEventEmitter, Platform } from 'react-native';
-import Regula from 'react-native-document-reader-api-beta';
+import { StyleSheet, View, Button, Text, Image, ScrollView, NativeEventEmitter, Platform, TouchableOpacity } from 'react-native';
+import Regula from '@regulaforensics/react-native-document-reader-api-beta';
 import * as RNFS from 'react-native-fs';
 import RadioGroup from 'react-native-radio-buttons-group';
 import ImagePicker from 'react-native-customized-image-picker';
+import * as Progress from 'react-native-progress';
 import CheckBox from 'react-native-check-box';
 
 const eventManager = new NativeEventEmitter(Regula.RNRegulaDocumentReader);
+const DocumentReader = Regula.DocumentReader;
+const Enum = DocumentReader.Enum;
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     eventManager.addListener('prepareDatabaseProgressChangeEvent', e => this.setState({ fullName: e["msg"] }));
-    eventManager.addListener('completionEvent', e => this.handleCompletion(Regula.DocumentReader.DocumentReaderCompletion.fromJson(JSON.parse(e["msg"]))));
+    eventManager.addListener('completionEvent', e => this.handleCompletion(DocumentReader.DocumentReaderCompletion.fromJson(JSON.parse(e["msg"]))));
     var licPath = Platform.OS === 'ios' ? (RNFS.MainBundlePath + "/regula.license") : "regula.license";
-    var readFile = Platform.OS === 'ios' ? RNFS.readFile : RNFS.readFileAssets; 
-    Regula.DocumentReader.prepareDatabase("Full", (respond) => {
+    var readFile = Platform.OS === 'ios' ? RNFS.readFile : RNFS.readFileAssets;
+    DocumentReader.prepareDatabase("Full", (respond) => {
       console.log(respond);
       readFile(licPath, 'base64').then((res) => {
         this.setState({ fullName: "Initializing..." });
-        Regula.DocumentReader.initializeReader(res, (respond) => {
+        DocumentReader.initializeReader(res, (respond) => {
           console.log(respond);
-          Regula.DocumentReader.isRFIDAvailableForUse((canRfid) => {
+          DocumentReader.isRFIDAvailableForUse((canRfid) => {
             if (canRfid) {
-              this.setState({ canRfid: true });
+              this.setState({ canRfid: true, rfidUIHeader: "Reading RFID", rfidDescription: "Place your phone on top of the NFC tag", rfidUIHeaderColor: "black" });
               this.setState({ canRfidTitle: '' });
             }
           }, error => console.log(error));
-          Regula.DocumentReader.getAvailableScenarios((jstring) => {
+          DocumentReader.getAvailableScenarios((jstring) => {
             var scenariosTemp = JSON.parse(jstring);
             var scenariosL = [];
             for (var i in scenariosTemp) {
               scenariosL.push({
-                label: Regula.DocumentReader.Scenario.fromJson(typeof scenariosTemp[i] === "string" ? JSON.parse(scenariosTemp[i]) : scenariosTemp[i]).name,
+                label: DocumentReader.Scenario.fromJson(typeof scenariosTemp[i] === "string" ? JSON.parse(scenariosTemp[i]) : scenariosTemp[i]).name,
                 value: i
               });
             }
@@ -48,7 +51,7 @@ export default class App extends Component {
                 this.setState({ selectedScenario: selectedItem })
               }} />
             });
-            Regula.DocumentReader.getDocumentReaderIsReady((isReady) => {
+            DocumentReader.getDocumentReaderIsReady((isReady) => {
               if (isReady)
                 this.setState({ fullName: "Ready" });
               else
@@ -73,43 +76,54 @@ export default class App extends Component {
   }
 
   handleCompletion(completion) {
-    console.log("action code: " + completion.action)
-    if (completion.action == Regula.DocumentReader.Enum.DocReaderAction.COMPLETE)
-      this.handleResults(completion.results)
-    if (completion.action == Regula.DocumentReader.Enum.DocReaderAction.CANCEL)
-      console.log("scanning canceled")
-    if (completion.action == Regula.DocumentReader.Enum.DocReaderAction.ERROR)
-      console.log("Error: " + completion.error.toString)
-    if (completion.action == Regula.DocumentReader.Enum.DocReaderAction.COMPLETE || Regula.DocumentReader.Enum.DocReaderAction.CANCEL || Regula.DocumentReader.Enum.DocReaderAction.ERROR)
+    if (this.state.isReadingRfid && completion.action === Enum.DocReaderAction.COMPLETE)
+      if (completion.results.rfidResult !== 1)
+        this.restartRfidUI();
+      else
+        this.hideRfidUI();
+    if (this.state.isReadingRfid && (completion.action === Enum.DocReaderAction.CANCEL || completion.action === Enum.DocReaderAction.ERROR))
       this.hideRfidUI()
-    if (completion.action == Regula.DocumentReader.Enum.DocReaderAction.NOTIFICATION && this.state.isReadingRfid)
-      updateRfidUI(completion.results)
+    if (this.state.isReadingRfid && completion.action === Enum.DocReaderAction.NOTIFICATION)
+      this.updateRfidUI(completion.results.documentReaderNotification)
+    if (completion.action === Enum.DocReaderAction.COMPLETE)
+      this.handleResults(completion.results)
   }
 
   showRfidUI() {
     // show animation
-    console.log("displaying rfid ui")
     this.setState({ isReadingRfid: true });
   }
 
   hideRfidUI() {
     // show animation
-    this.setState({ isReadingRfid: false });
+    this.restartRfidUI();
+    this.setState({ isReadingRfid: false, doRfid: false, rfidUIHeader: "Reading RFID", rfidUIHeaderColor: "black" });
+  }
+
+  restartRfidUI() {
+    this.setState({ rfidUIHeaderColor: "red", rfidUIHeader: "Failed!", runRfidProgressBar: false, rfidDescription: "Place your phone on top of the NFC tag", rfidProgress: -1, rfidSubprocessIsLong: false });
   }
 
   updateRfidUI(results) {
-    // update progress bar
+    this.setState({ rfidUIHeader: "Reading RFID", rfidUIHeaderColor: "black", runRfidProgressBar: true });
+    const loword = results.code & 0x0000FFFF;
+    if (isNaN(Enum.eRFID_DataFile_Type.getTranslation(loword)))
+      this.setState({ rfidDescription: Enum.eRFID_DataFile_Type.getTranslation(loword), rfidProgress: results.value / 100 });
+    if (results.value > 1 && results.value < 100)
+      this.setState({ rfidSubprocessIsLong: true });
+    else
+      this.setState({ rfidSubprocessIsLong: false });
   }
 
   displayResults(results) {
     this.setState({ fullName: "", docFront: require('./images/id.png'), portrait: require('./images/portrait.png') });
-    this.setState({ fullName: results.getTextFieldValueByType(Regula.DocumentReader.Enum.eVisualFieldType.FT_SURNAME_AND_GIVEN_NAMES) });
-    if (results.getGraphicFieldImageByType(Regula.DocumentReader.Enum.eGraphicFieldType.GF_DOCUMENT_IMAGE) != null) {
-      var base64DocFront = "data:image/png;base64," + results.getGraphicFieldImageByType(Regula.DocumentReader.Enum.eGraphicFieldType.GF_DOCUMENT_IMAGE);
+    this.setState({ fullName: results.getTextFieldValueByType(Enum.eVisualFieldType.FT_SURNAME_AND_GIVEN_NAMES) });
+    if (results.getGraphicFieldImageByType(Enum.eGraphicFieldType.GF_DOCUMENT_IMAGE) != null) {
+      var base64DocFront = "data:image/png;base64," + results.getGraphicFieldImageByType(Enum.eGraphicFieldType.GF_DOCUMENT_IMAGE);
       this.setState({ docFront: { uri: base64DocFront } });
     }
-    if (results.getGraphicFieldImageByType(Regula.DocumentReader.Enum.eGraphicFieldType.GF_PORTRAIT) != null) {
-      var base64Portrait = "data:image/png;base64," + results.getGraphicFieldImageByType(Regula.DocumentReader.Enum.eGraphicFieldType.GF_PORTRAIT);
+    if (results.getGraphicFieldImageByType(Enum.eGraphicFieldType.GF_PORTRAIT) != null) {
+      var base64Portrait = "data:image/png;base64," + results.getGraphicFieldImageByType(Enum.eGraphicFieldType.GF_PORTRAIT);
       this.setState({ portrait: { uri: base64Portrait } });
     }
   }
@@ -117,24 +131,24 @@ export default class App extends Component {
   handleResults(results) {
     if (this.state.doRfid && results != null && results.chipPage != 0) {
       accessKey = null;
-      accessKey = results.getTextFieldValueByType(Regula.DocumentReader.Enum.eVisualFieldType.FT_MRZ_STRINGS);
+      accessKey = results.getTextFieldValueByType(Enum.eVisualFieldType.FT_MRZ_STRINGS);
       if (accessKey != null && accessKey != "") {
         accessKey = accessKey.replace(/^/g, '').replace(/\n/g, '');
-        Regula.DocumentReader.setRfidScenario({
+        DocumentReader.setRfidScenario({
           mrz: accessKey,
-          pacePasswordType: Regula.DocumentReader.Enum.eRFID_Password_Type.PPT_MRZ,
+          pacePasswordType: Enum.eRFID_Password_Type.PPT_MRZ,
         }, e => { this.showRfidUI() }, error => console.log(error));
       } else {
         accessKey = null;
         accessKey = results.getTextFieldValueByType(159);
         if (accessKey != null && accessKey != "") {
-          Regula.DocumentReader.setRfidScenario({
+          DocumentReader.setRfidScenario({
             password: accessKey,
-            pacePasswordType: Regula.DocumentReader.Enum.eRFID_Password_Type.PPT_CAN,
-          }, e => { this.showRfidUI()  }, error => console.log(error));
+            pacePasswordType: Enum.eRFID_Password_Type.PPT_CAN,
+          }, e => { this.showRfidUI() }, error => console.log(error));
         }
       }
-      Regula.DocumentReader.readRFID(m => { }, e => console.log(e));
+      DocumentReader.readRFID(m => { }, e => console.log(e));
 
     } else
       this.displayResults(results);
@@ -143,16 +157,16 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        {this.state.isReadingRfid && <Text style={{
-            top: 1,
-            left: 1,
-            padding: 30,
-            fontSize: 20,
-          }}>
-            Reading RFID
-          </Text>
+        {this.state.isReadingRfid && <View style={styles.container}>
+          <Text style={{ paddingBottom: 30, fontSize: 23, color:this.state.rfidUIHeaderColor }}>{this.state.rfidUIHeader}</Text>
+          <Text style={{ paddingBottom: 50, fontSize: 20 }}>{this.state.rfidDescription}</Text>
+          <Progress.Bar width={200} useNativeDriver={true} indeterminateAnimationDuration={this.state.runRfidProgressBar ? 1000 : 0} color="#4285F4" indeterminate={!this.state.rfidSubprocessIsLong} progress={this.state.rfidProgress} />
+          <TouchableOpacity style={styles.cancelButton} onPress={()=>{this.hideRfidUI()}}>
+        <Text style={{fontSize:20}}>X</Text>
+      </TouchableOpacity>
+        </View>
         }
-        { !this.state.isReadingRfid && <View style={styles.container}>
+        {!this.state.isReadingRfid && <View style={styles.container}>
           <Text style={{
             top: 1,
             left: 1,
@@ -218,9 +232,9 @@ export default class App extends Component {
           </View>
 
           <View style={{ flexDirection: 'row' }}>
-            <Button
+            <Button color="#4285F4"
               onPress={() => {
-                Regula.DocumentReader.setConfig({
+                DocumentReader.setConfig({
                   functionality: {
                     videoCaptureMotionControl: true,
                     showCaptureButton: true
@@ -235,18 +249,18 @@ export default class App extends Component {
                   },
                 }, e => { }, error => console.log(error));
 
-                Regula.DocumentReader.showScanner(s => { }, e => console.log(e));
+                DocumentReader.showScanner(s => { }, e => console.log(e));
               }}
               title="Scan document"
             />
             <Text style={{ padding: 5 }}></Text>
-            <Button
+            <Button color="#4285F4"
               onPress={() => {
                 ImagePicker.openPicker({
                   multiple: true,
                   includeBase64: true
                 }).then(response => {
-                  Regula.DocumentReader.setConfig({
+                  DocumentReader.setConfig({
                     functionality: {
                       videoCaptureMotionControl: true,
                       showCaptureButton: true
@@ -267,7 +281,7 @@ export default class App extends Component {
                     images.push(response[i].data);
                   }
 
-                  Regula.DocumentReader.recognizeImages(images, s => this.handleResults(s), e => console.log(e));
+                  DocumentReader.recognizeImages(images, s => this.handleResults(s), e => console.log(e));
                 }).catch(e => {
                   console.log("ImagePicker: " + e);
                 });
@@ -284,6 +298,8 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    height: '100%',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -300,4 +316,9 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  cancelButton: {
+    position: 'absolute',
+    bottom:0,
+    right:20
+}
 });
